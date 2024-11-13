05e839a7015ff83b6c0d0486b5f75255fb271644
jubayerjuhan <davidjuhan23@gmail.com>
2024-11-14 00:06:32 +0600

    Added new email body ticket sending email

--- src/app_components/EmailSendingSection/EmailHtmlContent.ts
+++ src/app_components/EmailSendingSection/EmailHtmlContent.ts
@@ -104,48 +104,20 @@ export const itineraryHtmlContent = (link: string) => {
 //   </a></br>
 //   <p style="color: rgb(85,85,85); font-size: 16px; font-style:italic">(By acknowledging, you confirm that you have reviewed all the above details and agree with the provided information.)</p>
 // </div>
-export const ticketEmailContent = `<p style="margin-left:auto;"><span style="color: rgb(51,51,51);font-size: 16px;">Dear Customer</span><span style="color: rgb(51,51,51);font-size: 24px;">,</span>&nbsp;</p>
-<p><span style="color: rgb(85,85,85);">Thank you for choosing Airways Travel (Ticket Email).</span>&nbsp;</p>
-<p><span style="color: rgb(85,85,85);">Before finalizing your booking, we urge you to thoroughly check the details below for accuracy, including dates, names, and all applicable terms and conditions.</span> <br></p>
-<h2><span style="color: rgb(0,123,255);font-size: 20px;">Important Information Regarding Your Booking:</span>&nbsp;</h2>
-<ul>
-<li><span style="color: rgb(85,85,85);">Itinerary Overview: This booking includes 3 tickets as detailed below.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Ticket Validity: Tickets remain valid until the travel dates specified.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Baggage Allowance: Each Adult/Child is entitled to 2pc 23kg of each piece (46kg) of checked baggage plus one piece of cabin baggage not exceeding 7kg.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Date Change Charges: AUD 380 Plus fare and tax difference. No Show if applicable.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Cancellation Policy: AUD 550 Each ticket. Tickets are non-refundable in case of No-Show.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Seating Assignments: Seats will be allocated at the airport or at the time of check-in.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Passport Requirements: Passports must remain valid for at least 6 months beyond the return date of travel.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Visa and Immigration: It is the passenger's responsibility to ensure they have the required visa for the destination and any transit countries.</span> <br>&nbsp;</li>
-</ul>
-<p><span style="color: rgb(217,83,79);">*Please note*: Changes and cancellations to bookings must be made more than 72 hours prior to flight departure, as charges can vary within 72 hours of departure. Domestic tickets and partially used tickets are non-refundable.</span>&nbsp;</p>
-<p><span style="color: rgb(217,83,79);">*Airways Travel Disclaimer*: Airways Travel is not liable for any schedule changes or cancellations made by the airline.</span> <br></p>
-<h2><span style="color: rgb(0,123,255);font-size: 20px;">Flight Confirmation Reminder:</span>&nbsp;</h2>
-<p><span style="color: rgb(85,85,85);">We recommend you confirm your flight with us at least 72 hours prior to your departure. Please note that any request made within 48 hours of flight departure will not be guaranteed.</span>&nbsp;</p>
-<h2><span style="color: rgb(0,123,255);font-size: 20px;">Airport Arrival Recommendations:</span>&nbsp;</h2>
-<ul>
-<li><span style="color: rgb(85,85,85);">For International Travel: Reach the airport 3 hours before the flight departure time.</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">For Domestic Travel: Reach the airport 90 minutes before the flight departure time.</span>&nbsp;</li>
-</ul>
-<h2><span style="color: rgb(0,123,255);font-size: 20px;">Passenger Details:</span>&nbsp;</h2>
-<p><span style="color: rgb(85,85,85);">It is crucial that names are accurately reflected as per the passport. It is the passenger's responsibility to verify and ensure their name matches their passport.</span>&nbsp;</p>
-<ul>
-<li><span style="color: rgb(85,85,85);">First Name: Mohammad Mahbubur Mr</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Last Name: Rahman</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">First Name: Tasniya Tahsin Ms</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Last Name: Rahman</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">First Name: Jannatul Ms</span>&nbsp;</li>
-<li><span style="color: rgb(85,85,85);">Last Name: Ferdaus</span>&nbsp;</li>
-</ul>
-<h2><span style="color: rgb(0,123,255);font-size: 20px;">Itinerary</span>&nbsp;</h2>
-<p><span style="color: rgb(85,85,85);">1. Rahman, Mohammad Mahbubur Mr</span></p>
-<p><span style="color: rgb(85,85,85);">2. Rahman, Tasniya Tahsin Ms</span></p>
-<p><span style="color: rgb(85,85,85);">3. Ferdaus, Jannatul Ms</span>&nbsp;</p>
-<h2><br><span style="color: rgb(0,123,255);font-size: 20px;">Flight Details</span>&nbsp;</h2>
-<p><span style="color: rgb(85,85,85);">Sydney - Dhaka</span><br><br><span style="color: rgb(85,85,85);">           CZ 326</span><br><br><span style="color: rgb(85,85,85);">           DEPART: Kingsford Smith, Sydney (SYD)</span><br><br><span style="color: rgb(85,85,85);">           Saturday 23 November 2024, 11:25</span><br><br><span style="color: rgb(85,85,85);">           ARRIVE: Baiyun Intl, Guangzhou (CAN)</span><br><br><span style="color: rgb(85,85,85);">           Saturday 23 November 2024, 17:50</span><br><br><span style="color: rgb(85,85,85);">           Duration: 9h 25m | Cabin Class: Economy</span><br><br><span style="color: rgb(85,85,85);">           Airline: China Southern | Aircraft: Airbus A350</span> <br></p>
-<p><span style="color: rgb(85,85,85);">Return Flight: Dhaka - Sydney</span><br><br><span style="color: rgb(85,85,85);">           CZ 392</span><br><br><span style="color: rgb(85,85,85);">           DEPART: Hazrat Shahjalal Intl, Dhaka (DAC)</span><br><br><span style="color: rgb(85,85,85);">           Saturday 07 December 2024, 20:30</span><br><br><span style="color: rgb(85,85,85);">           ARRIVE: Kingsford Smith, Sydney (SYD)</span><br><br><span style="color: rgb(85,85,85);">           Sunday 08 December 2024, 17:25</span><br><br><span style="color: rgb(85,85,85);">           Duration: 12h 55m | Cabin Class: Economy</span><br><br><span style="color: rgb(85,85,85);">           Airline: China Southern | Aircraft: Airbus A35</span></p>
-<p></p>
-<p style="text-align:center;"><span style="color: rgb(153,153,153);font-size: 14px;">Thank you for choosing Airways Travel!</span></p>
-<p style="text-align:center;"><span style="color: rgb(153,153,153);font-size: 14px;">For any inquiries, please contact us at support@airwaystravel.com.</span><br>  <br></p>
+export const ticketEmailContent = `<p style="margin-left:auto; line-height: 34px">Dear Traveler,&nbsp;</p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);">Electronic Ticket Receipt is attached for you. Please take a printout of E-Ticket to present it when you check-in at the airport.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">NAME :</span> Please check your first name and surname as it must match with the passport and spellings should be correct.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">PASSPORT :</span> You are advised to have a passport valid for at least six months beyond the dates of your visit.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">VISA AND DOCUMENTATION RESPONSIBILITY :</span> It is your responsibility to ensure that you have the appropriate documentation including visas for the respective countries you intend to travel.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">INTERNATIONAL TRAVEL :</span> Reach airport 3 hours before the flight departure time.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">RECONFIRMATION :</span> We recommend you confirm your flight with us at least 72 hours prior to your departure . Any request within 48hrs of flight departure will not be guaranteed.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">HAND BAGGAGE :</span> You are allowed to carry one piece of 7kg in the hand carry.</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85);"><span style="font-weight: bold; text-decoration: underline">CONTACT DETAILS OF (ARRIVAL DESTINATION) :</span> You are requested to update your arrival destination contact details (Phone/Mobile number & Email Address) with us before departure (in case of return Air tickets)</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: red; margin-top: 10px">Travel advisories You are responsible for meeting all entry requirements for your destination. As travel regulations may change at short notice, we advise you to check the latest travel advisory.</span>&nbsp;<br/><br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85); margin-top: 10px">Regards,</span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85); margin-top: 10px">Airways Travel </span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85); margin-top: 10px">8 Tallis cct Truganina 3029 </span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85); margin-top: 10px">Toll Free 1300051525 </span>&nbsp;<br/></p>
+<p style="margin-left:auto; line-height: 34px"><span style="color: rgb(85,85,85); margin-top: 10px">Phone     03-90413975</span>&nbsp;<br/></p>
 
 `;
