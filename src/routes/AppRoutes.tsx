3f447f461b8d5b20740c00d535ff281ed247bfe2
jubayerjuhan <davidjuhan23@gmail.com>
2025-02-17 09:21:58 +0600

    site down fixed

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import {
   AcknowledgementPage,
@@ -82,135 +77,120 @@ const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* <Route Component={RefundFormPage} path={RefundPageURL} /> */}
-        {/* <Route Component={Login} path={LoginURL} /> */}
-        {/* <Route Component={ForgotPassword} path={ForgotPasswordURL} /> */}
-        {/* <Route Component={ResetPassword} path={ResetPasswordURL} /> */}
-        {/* <Route Component={AcknowledgementPage} path={AcknowledgementPageURL} /> */}
-        {/* <Route Component={PaymentProcessing} path={PaymentProcessingURL} /> */}
-        {/* <Route Component={MintpayPaymentPage} path={MintpayPaymentPageURL} /> */}
-        {/* <Route
+        <Route Component={RefundFormPage} path={RefundPageURL} />
+        <Route Component={Login} path={LoginURL} />
+        <Route Component={ForgotPassword} path={ForgotPasswordURL} />
+        <Route Component={ResetPassword} path={ResetPasswordURL} />
+        <Route Component={AcknowledgementPage} path={AcknowledgementPageURL} />
+        <Route Component={PaymentProcessing} path={PaymentProcessingURL} />
+        <Route Component={MintpayPaymentPage} path={MintpayPaymentPageURL} />
+        <Route
           Component={MintpaymentPaymentConfirmation}
           path={MintpayPaymentConfirmationURL}
-        /> */}
-        {/* <Route
+        />
+        <Route
           Component={PaymentMethodSelectPage}
           path={PaymentMethodSelectURL}
-        /> */}
-
-        {/* Private routes */}
-        {/* <Route
-          path={BaseURL}
-          element={<PrivateRoute component={Dashboard} />}
-        /> */}
-        {/* <Route path={HomepageURL} element={<PrivateRoute component={Home} />} /> */}
-        {/* <Route
-          path={LeadSuccessURL}
-          element={<PrivateRoute component={SuccessPage} />}
-        /> */}
-        {/* <Route
-          path={DashboardURL}
-          element={<PrivateRoute component={Dashboard} />}
-        /> */}
-        {/* <Route
-          path={UsersManagementURL}
-          element={<PrivateRoute component={Users} />}
-        /> */}
-        {/* <Route
-          path={FormFieldManagementURL}
-          element={<PrivateRoute component={FormManager} />}
-        /> */}
-        {/* <Route
-          path={FormPageURL}
-          element={<PrivateRoute component={ClientFormPage} />}
-        /> */}
-        {/* <Route
-          path={LeadsManagementURL}
-          element={<PrivateRoute component={Leads} />}
-        /> */}
-        {/* <Route
-          path={AllCustomersURL}
-          element={<PrivateRoute component={AllCustomers} />}
-        /> */}
-        {/* <Route
-          path={LeadDetailURL}
-          element={<PrivateRoute component={LeadDetailPage} />}
-        /> */}
-        {/* <Route
-          path={LeadSearchPageURL}
-          element={<PrivateRoute component={LeadSearch} />}
-        /> */}
-        {/* <Route
-          path={NewLeadsURL}
-          element={<PrivateRoute component={NewLeads} />}
-        /> */}
-        {/* <Route
-          path={MyLeadsURL}
-          element={<PrivateRoute component={MyCustomers} />}
-        /> */}
-        {/* <Route
-          path={ConvertedCustomersURL}
-          element={<PrivateRoute component={ConvertedCustomers} />}
-        /> */}
-        {/* <Route
-          path={SaleLostURL}
-          element={<PrivateRoute component={CancelledCustomers} />}
-        /> */}
-        {/* <Route
-          path={TestRouteURL}
-          element={<PrivateRoute component={TestPage} />}
-        /> */}
-        {/* <Route
-          path={RefundPageURL}
-          element={<PrivateRoute component={RefundFormPage} />}
-        /> */}
-        {/* <Route
-          path={RefundListURL}
-          element={<PrivateRoute component={RefundList} />}
-        /> */}
-        {/* <Route
-          path={MyFollowUpsURL}
-          element={<PrivateRoute component={MyFollowUps} />}
-        /> */}
-        {/* <Route
-          path={TotalFollowUpsURL}
-          element={<PrivateRoute component={TotalFollowups} />}
-        /> */}
-        {/* <Route
-          path={WhatsappLeadURL}
-          element={<PrivateRoute component={WhatsAppLeadPage} />}
-        /> */}
-        {/* <Route
-          path={WhatsappLeadListURL}
-          element={<PrivateRoute component={WhatsAppLeadsList} />}
-        /> */}
-        {/* <Route
-          path={WhatsappLeadDetailURL}
-          element={<PrivateRoute component={WhatsappLeadDetail} />}
-        /> */}
-        {/* <Route
-          path={FacebookLeadListURL}
-          element={<PrivateRoute component={FacebookLeadList} />}
-        /> */}
-        {/* <Route
-          path={CustomerHistoryURL}
-          element={<PrivateRoute component={CustomerHistory} />}
-        /> */}
-
-        {/* New homepage route */}
-        <Route
-          path="/"
-          element={
-            <div style={{ textAlign: "center", marginTop: "20%" }}>
-              <h1 style={{ fontSize: "48px", fontWeight: "bold" }}>
-                This site is shut down
-              </h1>
-            </div>
-          }
         />
 
-        {/* Redirect all other routes to the homepage */}
-        <Route path="*" element={<Navigate to="/" />} />
+        {/* Private routes */}
+        <Route
+          path={BaseURL}
+          element={<PrivateRoute component={Dashboard} />}
+        />
+        <Route path={HomepageURL} element={<PrivateRoute component={Home} />} />
+        <Route
+          path={LeadSuccessURL}
+          element={<PrivateRoute component={SuccessPage} />}
+        />
+        <Route
+          path={DashboardURL}
+          element={<PrivateRoute component={Dashboard} />}
+        />
+        <Route
+          path={UsersManagementURL}
+          element={<PrivateRoute component={Users} />}
+        />
+        <Route
+          path={FormFieldManagementURL}
+          element={<PrivateRoute component={FormManager} />}
+        />
+        <Route
+          path={FormPageURL}
+          element={<PrivateRoute component={ClientFormPage} />}
+        />
+        <Route
+          path={LeadsManagementURL}
+          element={<PrivateRoute component={Leads} />}
+        />
+        <Route
+          path={AllCustomersURL}
+          element={<PrivateRoute component={AllCustomers} />}
+        />
+        <Route
+          path={LeadDetailURL}
+          element={<PrivateRoute component={LeadDetailPage} />}
+        />
+        <Route
+          path={LeadSearchPageURL}
+          element={<PrivateRoute component={LeadSearch} />}
+        />
+        <Route
+          path={NewLeadsURL}
+          element={<PrivateRoute component={NewLeads} />}
+        />
+        <Route
+          path={MyLeadsURL}
+          element={<PrivateRoute component={MyCustomers} />}
+        />
+        <Route
+          path={ConvertedCustomersURL}
+          element={<PrivateRoute component={ConvertedCustomers} />}
+        />
+        <Route
+          path={SaleLostURL}
+          element={<PrivateRoute component={CancelledCustomers} />}
+        />
+        <Route
+          path={TestRouteURL}
+          element={<PrivateRoute component={TestPage} />}
+        />
+        <Route
+          path={RefundPageURL}
+          element={<PrivateRoute component={RefundFormPage} />}
+        />
+        <Route
+          path={RefundListURL}
+          element={<PrivateRoute component={RefundList} />}
+        />
+        <Route
+          path={MyFollowUpsURL}
+          element={<PrivateRoute component={MyFollowUps} />}
+        />
+        <Route
+          path={TotalFollowUpsURL}
+          element={<PrivateRoute component={TotalFollowups} />}
+        />
+        <Route
+          path={WhatsappLeadURL}
+          element={<PrivateRoute component={WhatsAppLeadPage} />}
+        />
+        <Route
+          path={WhatsappLeadListURL}
+          element={<PrivateRoute component={WhatsAppLeadsList} />}
+        />
+        <Route
+          path={WhatsappLeadDetailURL}
+          element={<PrivateRoute component={WhatsappLeadDetail} />}
+        />
+        <Route
+          path={FacebookLeadListURL}
+          element={<PrivateRoute component={FacebookLeadList} />}
+        />
+        <Route
+          path={CustomerHistoryURL}
+          element={<PrivateRoute component={CustomerHistory} />}
+        />
       </Routes>
     </Router>
   );
